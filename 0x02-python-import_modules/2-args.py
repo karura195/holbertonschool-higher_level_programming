1addad355999784d089f5c008361fa405bc93fac
karura195 <karla.diaz@pucp.pe>
2021-09-09 20:01:39 -0700

    Prints number of arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
         print("1 argument:")
     else:
         print("{:d} arguments:".format(len(sys.argv) - 1))
-    for i in range (1, len(sys.argv)):
+    for i in range(1, len(sys.argv)):
         print("{:d}: {:s}".format(i, sys.argv[i]))
