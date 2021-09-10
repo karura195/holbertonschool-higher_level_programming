f80a17058f4eed551ab2a66881e292bf4b19488d
karura195 <karla.diaz@pucp.pe>
2021-09-09 19:29:44 -0700

    Prints number of arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+if len(sys.argv) == 1:
+    print("0 arguments.")
+elif len(sys.argv) == 2:
+    print("1 argument:")
+else:
+    print("{:d} arguments:".format(len(sys.argv) - 1))
+for i in range (1, len(sys.argv)):
+    print("{:d}: {:s}".format(i, sys.argv[i]))
