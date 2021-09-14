acb8ff2afe3f7cdae8c92ceaa5e1b178044814b1
karura195 <karla.diaz@pucp.pe>
2021-09-13 21:49:41 -0700

    Multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is None:
-        length = 0
+    length = len(sentence)
+    if sentence == "":
         first = None
     else:
-        length = len(sentence)
         first = sentence[0]
     return length, first
