a2d23567d070745befd31a0ae6134ac524a539ec
karura195 <karla.diaz@pucp.pe>
2021-10-11 21:03:06 -0700

    Returns list object dir

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-"""Returns the list of available attributes and methods of an object"""
+"""Module"""
 
 
 def lookup(obj):
+    """
+    Returns the list of available attributes
+    and methods of an object
+    """
     return dir(obj)
