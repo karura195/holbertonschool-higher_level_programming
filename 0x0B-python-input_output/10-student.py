7d2699720bb081c787c8d20b38b2cd43d0a51b3c
karura195 <karla.diaz@pucp.pe>
2021-10-12 21:58:28 -0700

    Dict rep of class

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""class"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is not None:
+            dic = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    k[i] = self.__dict__[i]
+            return dic
+        return self.__dict__
