a7c83f786fcbd2f3ce156baf9d7e2e76834d8075
karura195 <karla.diaz@pucp.pe>
2021-10-12 21:46:47 -0700

    Adds args to a list

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''script'''
 
+
 import sys
 import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
@@ -10,8 +11,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 file1 = "add_item.json"
 my_list = []
 
-if os.path.isfile(file1):
-    my_list = load_from_json_file(file1)
+my_list = load_from_json_file(file1)
 
 for i in range(1, len(sys.argv)):
     my_list.append(sys.argv[i])
