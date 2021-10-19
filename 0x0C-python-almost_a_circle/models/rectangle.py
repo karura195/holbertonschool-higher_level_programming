44c91fbc4d4c54ea3652c43ff41414cb5c23acfa
karura195 <karla.diaz@pucp.pe>
2021-10-18 20:28:25 -0700

    Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -78,7 +78,11 @@ class Rectangle(Base):
 
     def display(self):
         """display Rectangle"""
+        for y in range(self.y):
+            print()
         for i in range(self.height):
+            for x in range(self.x):
+                print(" ",end="")
             for j in range(self.width):
                 print("#", end="")
             print()
