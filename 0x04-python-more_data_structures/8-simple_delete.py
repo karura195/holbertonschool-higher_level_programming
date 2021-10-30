645d18e67f1d67ffab2c4c3d483b5dd92f07f3fa
karura195 <karla.diaz@pucp.pe>
2021-10-29 17:04:26 -0700

    Deleting an element

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    new = a_dictionary
-    for i in new:
-        if key in new is True:
-            del new[key]
-            return new
-        else:
-            return new
+    a_dictionary.pop(key, None)
+    return a_dictionary
