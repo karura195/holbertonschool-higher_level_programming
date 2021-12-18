d924213adcc5a3064aa4a479319a14eeb3be970a
karura195 <karla.diaz@pucp.pe>
2021-12-17 18:35:56 -0800

    Lists states that begin with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
                          user=argv[1], passwd=argv[2],
                          db=argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'"+
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'" +
                    " ORDER BY id ASC")
     data = cursor.fetchall()
     for row in data:
