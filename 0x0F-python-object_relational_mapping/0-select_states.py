4983dd15dd20eb917b9af2af7cf53ffb186a35e8
karura195 <karla.diaz@pucp.pe>
2021-12-21 13:22:15 -0800

    Lists states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+""" Lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 from sys import argv
 
 
 if __name__ == "__main__":
-    """ Lists all states from the database hbtn_0e_0_usa """
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2],
                          db=argv[3], charset="utf8")
