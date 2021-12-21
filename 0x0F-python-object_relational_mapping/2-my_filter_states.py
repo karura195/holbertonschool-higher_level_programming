55754ac1eb42c2a6603e233a7b877ebf175eb175
karura195 <karla.diaz@pucp.pe>
2021-12-21 13:50:55 -0800

    Filter staters by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,8 +9,9 @@ if __name__ == "__main__":
                          user=argv[1], passwd=argv[2],
                          db=argv[3], charset="utf8")
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
-                   .format(argv[4]))
+    sql = "SELECT * FROM states WHERE name COLLATE utf8mb4_bin = '{}' "
+    sql += "ORDER BY id ASC"
+    cursor.execute(sql.format(argv[4]))
     data = cursor.fetchall()
     for row in data:
         print(row)
