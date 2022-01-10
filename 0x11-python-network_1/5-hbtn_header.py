da7caa38349930f1b551e131f65b62008288e218
karura195 <karla.diaz@pucp.pe>
2022-01-09 20:46:05 -0800

    Response header value

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Displays the value of the variable X-Request-Id in the response header
+"""
+
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get(argv[1])
+    print(r.headers.get('X-Request-Id'))
