af4d03f9d523144491789047995ba017f944e033
karura195 <karla.diaz@pucp.pe>
2022-01-09 18:08:39 -0800

    Finds a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def find_peak(list_of_integers):
+    arr = list_of_integers
+    n = len(arr)
+    if n == 0:
+        return None
+    mid = n // 2
+    if (mid == n - 1 or arr[mid] >= arr[mid + 1]) and (mid == 0 or arr[mid] >=
+                                                       arr[mid - 1]):
+        return arr[mid]
+    if mid != n - 1 and arr[mid + 1] > arr[mid]:
+        return find_peak(arr[mid + 1:])
+    return find_peak(arr[:mid])
