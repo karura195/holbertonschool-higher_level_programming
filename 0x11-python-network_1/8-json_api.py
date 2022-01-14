a19a74698f3eeb6240935d62c5d81b1e795e9111
karura195 <karla.diaz@pucp.pe>
2022-01-13 16:37:26 -0800

    first commit

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter
+in variable "q"
+"""
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    arg = ""
+    if len(argv) == 2:
+        arg = argv[1]
+    try:
+        r = requests.post('http://0.0.0.0:5000/search_user', data={'q': arg})
+        j = r.json()
+        if j:
+            print('[{}] {}'.format(j.get('id'), j.get('name')))
+        else:
+            print('No result')
+    except ValueError:
+        print('Not a valid JSON')
