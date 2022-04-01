d5511164893b2ae4a4cb1f7a6462b10299810b42
karura195 <karla.diaz@pucp.pe>
2022-04-01 10:18:49 -0700

    Prints n times C is fun

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const { argv } = require('process');
 const n = parseInt(argv[2]);
-for (let i = 0; i < n; i++){
-	console.log('C is fun');
+for (let i = 0; i < n; i++) {
+  console.log('C is fun');
 }
