815d2320d1fa3de9654243053f187d23ca83353f
karura195 <karla.diaz@pucp.pe>
2022-04-01 10:07:33 -0700

    Prints n times C is fun

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const { argv } = require('process');
+const n = parseInt(argv[2]);
+
+if (isNaN(num)) {
+  console.log('Missing number of occurrences');
+}
+else if (argv[2] > 0) {
+	for (let i = 0; i < n; i++) {
+		console.log('C is fun');
+	}
+}
