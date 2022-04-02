c2e57b77658fc39e3388fbc60fe5a745f5bbd808
karura195 <karla.diaz@pucp.pe>
2022-04-01 22:36:57 -0700

    Prints state with arg

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import sessionmaker
 from sys import argv
 
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     sql = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
     engine = create_engine(sql.format(argv[1],
                                       argv[2], argv[3]), pool_pre_ping=True)
