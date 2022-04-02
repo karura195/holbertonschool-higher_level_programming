4866f87d9d85f6eda32c210de56fbe70e8c5d0f5
karura195 <karla.diaz@pucp.pe>
2022-04-01 19:58:40 -0700

    State class

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" Contains the class definition of a State and an
-instance Base = declarative_base() """
+""" Contains the class definition of a State """
 from sqlalchemy import Column, Integer, String
 from model_state import Base
 from sqlalchemy import ForeignKey
