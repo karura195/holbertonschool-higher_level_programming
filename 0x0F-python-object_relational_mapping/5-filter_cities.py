99b3523d7bbe4e6566397abaf8a2085cbd42854e
karura195 <karla.diaz@pucp.pe>
2022-04-01 19:16:07 -0700

    List cities of the passed arg

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -4,15 +4,14 @@ import MySQLdb
 
 if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
+                         password=argv[2], db=argv[3], charset="utf8")
     state = argv[4]
     cur = db.cursor()
-    sql = """SELECT cities.name
-          FROM cities JOIN states
-          ON cities.state_id = states.id
-          WHERE states.name = %s ORDER by cities.id ASC"""
-    params = (state,)
-    cur.execute(sql, params)
+    sql = "SELECT cities.name "
+    sql += "FROM cities JOIN states "
+    sql += "ON cities.state_id = states.id "
+    sql += "WHERE states.name = '{state}' ORDER by cities.id ASC"
+    cur.execute(sql.format(state))
     query_rows = cur.fetchall()
 
     print(", ".join(row[0] for row in query_rows))
