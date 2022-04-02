b1fdf2d2a05e03dfbfda131616dc44d6d4699fca
karura195 <karla.diaz@pucp.pe>
2022-04-01 19:24:28 -0700

    List cities of the passed arg

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+""" Takes in the name of a state as an argument and lists
+all cities of that state """
 from sys import argv
 import MySQLdb
 
+
 if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         password=argv[2], db=argv[3], charset="utf8")
+                         passwd=argv[2], db=argv[3], charset="utf8")
     state = argv[4]
     cur = db.cursor()
     sql = "SELECT cities.name "
